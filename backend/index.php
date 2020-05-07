ef9f7f80a41a555ddf4b8b06d948f40a5471df3d
jahzan99 <45985073+jahzan99@users.noreply.github.com>
2020-05-07 10:10:30 +0530

    backend change

--- backend/index.php
+++ backend/index.php
@@ -6,9 +6,8 @@ $name = $_POST['name'];
 $age = $_POST['age'];
 $email = $_POST['email'];
 $contact = $_POST['contact'];
-$check = $_POST['check'];
 
-if(empty($name) or empty($age) or empty($email) or empty($contact) or empty($check)){
+if(empty($name) or empty($age) or empty($email) or empty($contact)){
     echo 2;
 }else{
     $sql = 'INSERT INTO competitors(cname,cage,cemail,ccontact) VALUES(:cname,:cage,:cemail,:ccontact)';
