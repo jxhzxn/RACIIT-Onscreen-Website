772976d851e25cf2be76b95ef05532775a936c75
Jahzan Ahamed <jahzanahamed99@gmail.com>
2020-05-26 23:10:33 +0530

    can upload

--- backend/index.php
+++ backend/index.php
@@ -19,7 +19,7 @@ if(empty($name) or empty($age) or empty($email) or empty($contact) or empty($nic
     $sql = 'INSERT INTO competitors(cname,cage,cemail,ccontact,cnic) VALUES(:cname,:cage,:cemail,:ccontact,:cnic)';
     $stmt = $con->prepare($sql);
     $stmt->execute(['cname'=>$name,'cage'=>$age,'cemail'=>$email,'ccontact'=>$contact,'cnic'=>$nic]);
-    // move_uploaded_file($file_tmp,$location.$newfilename);
+    move_uploaded_file($file_tmp,$location.$newfilename);
     echo 1;
 }
 
